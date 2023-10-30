175d38d0c788a0a88ed0eddb6ea3b80bbee75c38
keyla <keyyleiva@gmail.com>
2023-10-30 19:20:44 -0400

    completed lab

--- server/app.py
+++ server/app.py
@@ -4,6 +4,8 @@ from werkzeug.exceptions import NotFound
 
 from config import app
 
+from models import db, User, NationalPark, UserVisitedPark
+
 # unique error message upon nonexistent server-side route
 @app.errorhandler(NotFound)
 def route_not_found(e):
@@ -17,6 +19,94 @@ def route_not_found(e):
 @app.route('/')
 def home():
     return ""
+
+@app.route('/users', methods = ['GET', 'POST'])
+def users():
+
+    if request.method == 'GET':
+        users = User.query.all()
+        #rules removes park of the list displayed or object 
+        users_dict = [user.to_dict(rules = ('-user_visited_park', '-password' )) for user in users]
+
+        return make_response(users_dict, 200)
+    
+    elif request.method == 'POST':
+        form_data = request.get_json()
+
+        try:
+
+            new_user_obj = User(
+                username = form_data['username'],
+                passowrd = form_data['password']
+            )
+
+            db.session.add(new_user_obj)
+
+            db.session.commit()
+            
+            new_user_obj_dict = new_user_obj.to_dict()
+
+            return make_response(new_user_obj_dict, 201)
+        
+        except ValueError:
+
+            return make_response({ "Username must be between 4 and 14 characrters long, inclusive!" : None }, 403)
+
+@app.route('/national_parks', methods = ['GET'])
+def national_parks():
+    national_parks = NationalPark.query.all()
+
+    national_parks_dict = [national_park.to_dict() for national_park in national_parks]
+
+    return make_response(national_parks_dict, 200)
+
+@app.route('/users/<int:id>', methods = ['GET', 'PATCH'])
+def user_by_id(id):
+    user_by_id = User.query.filter_by(id = id).first()
+    #User.query.filter(User.id == id).first()
+    if user_by_id:
+        if request.method == 'GET':
+            user_dict = user_by_id.to_dict(rules = ('-user_visited_park', '-password' ))
+
+            return make_response(user_dict, 200)
+            #return make_response(user_by_id.to_dict(), 200)
+
+        elif request.method == 'PATCH':
+            form_data = request.get_json()
+
+            try:
+                for attr in form_data:
+                    setattr(user_by_id, attr, form_data.get(attr))
+                
+                db.session.commit()
+
+                return make_response(user_by_id.to_dict(), 201)
+            
+            except ValueError:
+
+                return make_response({ "Username must be between 4 and 14 characrters long, inclusive!" : None }, 403)
+    else: 
+        return make_response("User not found!", 404)
+    
+@app.route('/national_parks/<int:id>', methods = ['DELETE'])
+def national_park_by_id(id):
+    park_by_id = NationalPark.query.filter_by(id = id).first()
+
+    if park_by_id:
+
+        associated_visits = UserVisitedPark.query.filter(UserVisitedPark.park_id == id).all()
+
+        for associated_visit in associated_visits:
+            db.session.delete(associated_visit)
+
+        db.session.delete(park_by_id)
+
+        db.session.commit()
+
+        return make_response({}, 202)
+    else:
+        return make_response("Park not found!", 404)
+
 
 if __name__ == '__main__':
     app.run(port = 5555, debug = True)
--- server/models.py
+++ server/models.py
@@ -10,3 +10,54 @@ metadata = MetaData(naming_convention = {
 
 # creates SQLAlchemy DB ORM
 db = SQLAlchemy(metadata = metadata)
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    serialize_rules = ('-user_visited_park.user', )
+
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String, unique = True)
+    password = db.Column(db.String)
+
+    created_at = db.Column(db.DateTime, server_default = db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
+
+    user_visited_park = db.relationship('UserVisitedPark', back_populates = 'user')
+
+    #validation
+    @validates('username')
+    def validates_username(self, key, username):
+        if 4 <= len(username) <= 14:
+            return username
+        else:
+            raise ValueError("Username must be between 4 and 14 characters, inclusive!")
+
+class NationalPark(db.Model, SerializerMixin):
+    __tablename__ = 'national_parks'
+
+    serialize_rules = ('-user_visited_park.national_park', )
+
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String)
+    state = db.Column(db.String)
+
+    created_at = db.Column(db.DateTime, server_default = db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
+
+    user_visited_park = db.relationship('UserVisitedPark', back_populates = ('national_park'))
+
+class UserVisitedPark(db.Model, SerializerMixin):
+    __tablename__ = 'user_visited_park'
+
+    serialize_rules = ('-user.user_visited_park', '-national_park.user_visited_park' )
+
+
+    id = db.Column(db.Integer, primary_key = True)
+    date_of_visit = db.Column(db.String)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    park_id = db.Column(db.Integer, db.ForeignKey('national_parks.id'))
+
+    user = db.relationship('User', back_populates = ('user_visited_park'))
+    national_park = db.relationship('NationalPark', back_populates = ('user_visited_park'))
